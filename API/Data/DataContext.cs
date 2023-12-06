55a23e1206d36d5a5c95379c975dfac8ceedf240
Nguyen Thanh Duy <duynt0319@gmail.com>
2023-12-06 14:01:13 +0700

    Add project files.

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AppUser>().HasData(
+                new AppUser { Id = 1, UserName = "John", },
+                new AppUser { Id = 2, UserName = "Duy", }
+            );
+        }
+
+    }
+}
---
+++ API/Migrations/20231205075728_seedData.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class seedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "UserName" },
+                values: new object[,]
+                {
+                    { 1, "John" },
+                    { 2, "Duy" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
